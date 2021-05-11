1021f1e382c9d67dbb028a9a57922739faf916f0
athelia <athelia.hk@gmail.com>
2021-05-11 15:49:31 -0400

    Create App component

--- server.py
+++ server.py
@@ -1,7 +1,13 @@
-from Flask import flask
+from flask import Flask, render_template, jsonify
 
-app = Flask()
+app = Flask(__name__)
+
 
 @app.route('/')
 def homepage():
     """Render app."""
+    return render_template('app.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port='5003')
