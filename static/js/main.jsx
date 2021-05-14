3cd345368108d2886f941cef1d3645bb23fca362
athelia <athelia.hk@gmail.com>
2021-05-14 15:40:40 -0400

    Add Cards Remaining counters for size of deck and cards left on table

--- static/js/main.jsx
+++ static/js/main.jsx
@@ -14,10 +14,26 @@ function Main(){
     updateCardsInPlay(deck.slice(0, 16));
     updateDeck(deck.slice(16, deck.length));
   }
+  
+  let nullCount = 0;
+  // let setOfCards = new Set();
+  let setOfCards = new Set(cardsInPlay);
+  React.useEffect(() => {
+    for(const card of cardsInPlay){
+      if(card === null){
+        nullCount++;
+      }
+    }
+    
+  }, [cardsInPlay])
 
   if (playing){
     return(
       <React.Fragment>
+        <p id='deck-count'>{deck.length} Cards in Deck</p>
+        
+        <p id='table-count'>{cardsInPlay.length - nullCount} Cards on Table</p>
+        {/* <p id='table-count'>{setOfCards.size - 1} Cards on Table</p> */}
         <PlayArea 
           deck={deck} 
           updateDeck={updateDeck}
